489815f6c359a195aca6de92cbca211d076ba1e5
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-11-14 21:10:45 +0900

    feat: StationListPage 기본 UI 레이아웃 추가

--- lib/pages/station/stations_list.dart
+++ lib/pages/station/stations_list.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
 
+// command + shift + A : 다크모드
+
 class StationListPage extends StatelessWidget {
+  List<String> stationList = [
+    "수서",
+    "동탄",
+    "평택지제",
+    "천안아산",
+    "오송",
+    "대전",
+    "김천구미",
+    "동대구",
+    "경주",
+    "울산",
+    "부산",
+  ];
+
+  Widget stationListView() {
+    return ListView.builder(
+        itemCount: stationList.length,
+        itemBuilder: (ctx, idx) {
+          return ListTile(title: Text(stationList[idx]));
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('출발역'),
-      ),
-      body: Text('StationListPage'),
-    );
+        appBar: AppBar(
+          title: Text('출발역'),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios),
+            onPressed: () {},
+          ),
+        ),
+        body: Expanded(
+          child: stationListView(),
+        ));
   }
 }
