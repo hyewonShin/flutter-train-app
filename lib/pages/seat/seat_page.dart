e2790e69decb428ca256356ff20064db427fbb6b
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-11-15 23:12:08 +0900

    임시

--- lib/pages/seat/seat_page.dart
+++ lib/pages/seat/seat_page.dart
@@ -9,126 +9,135 @@ class SeatPage extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text(
-                  '수서',
-                  style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: const Color.fromARGB(255, 89, 76, 92)),
-                ),
-                Icon(
-                  Icons.arrow_circle_right_outlined,
-                  size: 30,
-                ),
-                Text(
-                  '부산',
-                  style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.purple),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 24,
-                    height: 24,
-                    child: DecoratedBox(
-                      decoration: BoxDecoration(
-                          color: Colors.purple,
-                          borderRadius: BorderRadius.circular(8)),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 4,
-                  ),
-                  Text('선택됨'),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  SizedBox(
-                    width: 24,
-                    height: 24,
-                    child: DecoratedBox(
-                        decoration: BoxDecoration(
-                            color: Colors.grey[300]!,
-                            borderRadius: BorderRadius.circular(8))),
-                  ),
-                  SizedBox(
-                    width: 4,
-                  ),
-                  Text('선택안됨'),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 600,
-              child: ListView(children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ColumnSeat(),
-                    SizedBox(
-                      width: 4,
-                    ),
-                    ColumnSeat(),
-                    SizedBox(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text('1'),
-                          Text('2'),
-                          Text('3'),
-                        ],
-                      ),
-                    ),
-                    ColumnSeat(),
-                    SizedBox(
-                      width: 4,
-                    ),
-                    ColumnSeat(),
-                  ],
-                ),
-              ]),
-            ),
-            SizedBox(
-              width: 380,
-              height: 60,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.purple,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20))),
-                onPressed: () {},
-                child: Text(
-                  '예매 하기',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold),
+            PickedStation(),
+            IsPickExample(),
+            RowTitle(),
+            Flexible(
+              child: ListView(
+                children: List.generate(
+                  20,
+                  (index) => RowSeat(),
                 ),
               ),
-            )
+            ),
+            ReservationBtn()
           ],
         ));
   }
 }
 
-class ColumnSeat extends StatelessWidget {
-  const ColumnSeat({
+class PickedStation extends StatelessWidget {
+  const PickedStation({
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Text(
+          '수서',
+          style: TextStyle(
+              fontSize: 30, fontWeight: FontWeight.bold, color: Colors.purple),
+        ),
+        Icon(
+          Icons.arrow_circle_right_outlined,
+          size: 30,
+        ),
+        Text(
+          '부산',
+          style: TextStyle(
+              fontSize: 30, fontWeight: FontWeight.bold, color: Colors.purple),
+        ),
+      ],
+    );
+  }
+}
+
+class ReservationBtn extends StatelessWidget {
+  const ReservationBtn({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 40),
+      child: SizedBox(
+        width: 380,
+        height: 60,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.purple,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20))),
+          onPressed: () {},
+          child: Text(
+            '예매 하기',
+            style: TextStyle(
+                color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class IsPickExample extends StatelessWidget {
+  const IsPickExample({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: 24,
+            height: 24,
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                  color: Colors.purple, borderRadius: BorderRadius.circular(8)),
+            ),
+          ),
+          SizedBox(
+            width: 4,
+          ),
+          Text('선택됨'),
+          SizedBox(
+            width: 20,
+          ),
+          SizedBox(
+            width: 24,
+            height: 24,
+            child: DecoratedBox(
+                decoration: BoxDecoration(
+                    color: Colors.grey[300]!,
+                    borderRadius: BorderRadius.circular(8))),
+          ),
+          SizedBox(
+            width: 4,
+          ),
+          Text('선택안됨'),
+        ],
+      ),
+    );
+  }
+}
+
+class RowTitle extends StatelessWidget {
+  const RowTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         SizedBox(
           width: 50,
@@ -139,25 +148,72 @@ class ColumnSeat extends StatelessWidget {
             style: TextStyle(fontSize: 18),
           ),
         ),
+        SizedBox(
+          width: 50,
+          height: 50,
+          child: Text(
+            'B',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+        SizedBox(
+          width: 50,
+          height: 50,
+        ),
+        SizedBox(
+          width: 50,
+          height: 50,
+          child: Text(
+            'C',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+        SizedBox(
+          width: 50,
+          height: 50,
+          child: Text(
+            'D',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class RowSeat extends StatelessWidget {
+  const RowSeat({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
         seat(),
+        SizedBox(
+          width: 4,
+        ),
         seat(),
+        SizedBox(
+          width: 50,
+          height: 50,
+          child: Center(
+            child: Text(
+              '1',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+        ),
         seat(),
-        seat(),
-        seat(),
-        seat(),
-        seat(),
-        seat(),
-        seat(),
-        seat(),
-        seat(),
-        seat(),
-        seat(),
-        seat(),
-        seat(),
-        seat(),
-        seat(),
-        seat(),
-        seat(),
+        SizedBox(
+          width: 4,
+        ),
         seat(),
       ],
     );
@@ -171,7 +227,7 @@ class seat extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Row(
       children: [
         SizedBox(
           width: 50,
@@ -181,9 +237,6 @@ class seat extends StatelessWidget {
                 color: Colors.grey[300]!,
                 borderRadius: BorderRadius.circular(8)),
           ),
-        ),
-        SizedBox(
-          height: 4,
         ),
       ],
     );
