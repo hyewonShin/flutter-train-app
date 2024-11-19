6727c91e5792942b7bb2f5aa2b55c0ab8ab014f1
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-11-19 15:19:26 +0900

    refactor: 다크모드 appBar 폰트 크기 변경

--- lib/them.dart
+++ lib/them.dart
@@ -6,7 +6,7 @@ final darkTheme = ThemeData(
     brightness: Brightness.dark, // 밝기 설정
     scaffoldBackgroundColor: const Color.fromARGB(221, 71, 47, 47),
     appBarTheme: AppBarTheme(
-        titleTextStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        titleTextStyle: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
     textTheme: const TextTheme(
       bodyMedium: TextStyle(color: Colors.black),
       bodyLarge: TextStyle(
